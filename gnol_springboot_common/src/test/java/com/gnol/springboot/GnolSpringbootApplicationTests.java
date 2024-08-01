d0b730321d3fe9ae4e39e073ada2e30285e8ab71
Klauspilathp <zhouchekui05492@126.com>
2024-08-02 06:56:54 +0800

    初始化

---
+++ gnol_springboot_common/src/test/java/com/gnol/springboot/GnolSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gnol.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GnolSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ gnol_springboot_core/src/main/java/com/gnol/springboot/GnolSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.gnol.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GnolSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GnolSpringbootApplication.class, args);
+    }
+
+}
