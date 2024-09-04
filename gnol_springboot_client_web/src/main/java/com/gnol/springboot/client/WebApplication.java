689c0b00a01a7ca4215140000604a1a2ca12a83f
Klauspilathp <zhouchekui05492@126.com>
2024-09-04 22:21:24 +0800

    1

--- gnol_springboot_client_web/src/main/java/com/gnol/springboot/client/WebApplication.java
+++ gnol_springboot_client_web/src/main/java/com/gnol/springboot/client/WebApplication.java
@@ -2,12 +2,19 @@ package com.gnol.springboot.client;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class WebApplication {
+public class WebApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(WebApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(WebApplication.class);
     }
 
 }
