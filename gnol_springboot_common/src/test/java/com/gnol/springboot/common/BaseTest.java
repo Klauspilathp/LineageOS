622ad671416f29e9ca829bbceed0e487de994442
Klauspilathp <zhouchekui05492@126.com>
2024-09-17 02:50:24 +0800

    1

--- gnol_springboot_common/src/test/java/com/gnol/springboot/common/BaseTest.java
+++ gnol_springboot_common/src/test/java/com/gnol/springboot/common/BaseTest.java
@@ -3,6 +3,13 @@ package com.gnol.springboot.common;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+/**
+ * @Title: BaseTest
+ * @Package: com.gnol.springboot.common
+ * @author: 吴佳隆
+ * @date: 2020/6/6 19:39
+ * @Description: 基础测试类
+ */
 @SpringBootTest
 class BaseTest {
 
