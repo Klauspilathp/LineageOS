b9d00cd50e0595079dda3e62932bdf3da4824ad0
Klauspilathp <zhouchekui05492@126.com>
2024-12-11 23:29:24 +0800

    1

---
+++ gnol_springboot_eureka_server/src/main/java/com/gnol/springboot/eureka/server/EurekaServerApplication.java
@@ -0,0 +1,22 @@
+package com.gnol.springboot.eureka.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @Title: EurekaServerApplication
+ * @Package: com.gnol.springboot.eureka.server
+ * @author: 吴佳隆
+ * @date: 2020年6月11日 下午7:23:03
+ * @Description: eureka server 启动类
+ */
+@SpringBootApplication(scanBasePackages = {"com.gnol.springboot"})
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+
+}
