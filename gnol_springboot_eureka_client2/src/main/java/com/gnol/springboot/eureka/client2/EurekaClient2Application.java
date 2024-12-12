b3f09f724ccf7ff8886dc64b6d57e35ebbf60aef
Klauspilathp <zhouchekui05492@126.com>
2024-12-12 11:48:54 +0800

    1

---
+++ gnol_springboot_eureka_client2/src/main/java/com/gnol/springboot/eureka/client2/EurekaClient2Application.java
@@ -0,0 +1,22 @@
+package com.gnol.springboot.eureka.client2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @Title: EurekaClient2Application
+ * @Package: com.gnol.springboot.eureka.client2
+ * @author: 吴佳隆
+ * @date: 2020年6月12日 上午8:11:59
+ * @Description: eureka client 启动类
+ */
+@SpringBootApplication(scanBasePackages = {"com.gnol.springboot"})
+@EnableDiscoveryClient
+public class EurekaClient2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaClient2Application.class, args);
+    }
+
+}
