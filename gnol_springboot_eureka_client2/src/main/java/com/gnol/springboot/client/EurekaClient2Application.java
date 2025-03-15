cd50bd97d318c44c0ba849c34c9be16ffb5c1e72
Klauspilathp <zhouchekui05492@126.com>
2025-03-15 22:05:55 +0800

    1

--- gnol_springboot_eureka_client2/src/main/java/com/gnol/springboot/client/EurekaClient2Application.java
+++ gnol_springboot_eureka_client2/src/main/java/com/gnol/springboot/client/EurekaClient2Application.java
@@ -2,6 +2,7 @@ package com.gnol.springboot.client;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
@@ -19,6 +20,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
  */
 @EnableDiscoveryClient
 @EnableFeignClients // 启用 Fegin
+@EnableCircuitBreaker // 启用 hystrix
 public class EurekaClient2Application {
 
     public static void main(String[] args) {
