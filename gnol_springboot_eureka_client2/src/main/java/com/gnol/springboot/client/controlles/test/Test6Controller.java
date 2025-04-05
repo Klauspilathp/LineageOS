5c6589df340e0ef47fabf84697321f0b44ef52a6
Klauspilathp <zhouchekui05492@126.com>
2025-04-05 16:41:25 +0800

    1

---
+++ gnol_springboot_eureka_client2/src/main/java/com/gnol/springboot/client/controlles/test/Test6Controller.java
@@ -0,0 +1,96 @@
+package com.gnol.springboot.client.controlles.test;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.gnol.plugins.core.PageResult;
+
+/**
+ * @Title: Test6Controller
+ * @Package: com.gnol.springboot.client.controlles.test
+ * @author: 吴佳隆
+ * @date: 2020年6月19日 下午6:23:49
+ * @Description: Feign 整合 Hystrix
+ */
+@RestController
+@RequestMapping("/test6")
+public class Test6Controller {
+    private static final Logger logger = LoggerFactory.getLogger(Test6Controller.class);
+    @Autowired
+    private Client1Test2FeignClient feignClient;
+
+    @RequestMapping(value = "/t1", method = RequestMethod.GET)
+    public PageResult t1(String id) {
+        logger.info("client2.t1 param id : {}", id);
+        return feignClient.t1(id);
+    }
+
+    @GetMapping(value = "/t2")
+    public PageResult t2() {
+        return feignClient.t2();
+    }
+
+}
+
+/**
+ * @Title: Client1Test2FeignClient
+ * @Package: com.gnol.springboot.client.controlles.test
+ * @author: 吴佳隆
+ * @date: 2020年6月19日 上午10:21:54
+ * @Description: Feign 客户端
+ */
+@FeignClient(name = "gnol-springboot-eureka-client1", // 服务应用名
+        path = "/client1/test2", // 服务路径前缀
+        fallback = Client1Test2FeignClientFallback.class // 回调函数类
+// configuration = FeignDisableHystrixConfiguration.class // 为当前 Feign 禁用 Hystrix 功能
+)
+interface Client1Test2FeignClient extends Client1Test2Mapping {
+
+}
+
+/**
+ * @Title: Client1Test2Mapping
+ * @Package: com.gnol.springboot.client.controlles.test
+ * @author: 吴佳隆
+ * @date: 2020年6月19日 上午10:23:02
+ * @Description: client1 的 test2 类服务接口
+ */
+interface Client1Test2Mapping {
+
+    @RequestMapping(value = "/t1", method = RequestMethod.GET)
+    PageResult t1(@RequestParam("id") String id);
+
+    @GetMapping("/t2")
+    PageResult t2();
+
+}
+
+/**
+ * @Title: Client1Test2FeignClientFallback
+ * @Package: com.gnol.springboot.client.controlles.test
+ * @author: 吴佳隆
+ * @date: 2020年6月19日 下午7:04:53
+ * @Description: Hystrix 降级方法
+ */
+@Component
+class Client1Test2FeignClientFallback implements Client1Test2FeignClient {
+
+    @Override
+    public PageResult t1(String id) {
+        return PageResult.error("id 为 " + id + " 的 t1 服务被降级了！！！");
+    }
+
+    @Override
+    public PageResult t2() {
+        return PageResult.error("t2 服务被降级了！！！");
+    }
+
+}
