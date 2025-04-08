7a8edfb8f9e51432fe8765ca1669ce9560a688e8
Klauspilathp <zhouchekui05492@126.com>
2025-04-08 12:27:55 +0800

    1

--- gnol_springboot_admin/src/main/java/com/gnol/springboot/admin/AdminApplication.java
+++ gnol_springboot_admin/src/main/java/com/gnol/springboot/admin/AdminApplication.java
@@ -3,6 +3,7 @@ package com.gnol.springboot.admin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
 /**
  * @Title: ConfigApplication
@@ -13,6 +14,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
  */
 @SpringBootApplication(scanBasePackages = {"com.gnol.springboot"})
 @EnableEurekaClient
+@EnableTurbine // turbine
 public class AdminApplication {
 
     public static void main(String[] args) {
