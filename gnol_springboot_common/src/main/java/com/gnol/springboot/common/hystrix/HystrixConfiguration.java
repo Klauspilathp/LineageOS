ad2831df707db584400c235b086022d1cce75519
Klauspilathp <zhouchekui05492@126.com>
2025-04-21 13:49:55 +0800

    1

---
+++ gnol_springboot_common/src/main/java/com/gnol/springboot/common/hystrix/HystrixConfiguration.java
@@ -0,0 +1,29 @@
+package com.gnol.springboot.common.hystrix;
+
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
+
+/**
+ * @Title: HystrixConfiguration
+ * @Package: com.gnol.springboot.common.hystrix
+ * @author: 吴佳隆
+ * @date: 2020年6月20日 下午4:55:20
+ * @Description: TODO
+ */
+@Configuration
+public class HystrixConfiguration {
+
+    @Bean
+    public ServletRegistrationBean<HystrixMetricsStreamServlet> hystrixMetricsStreamServlet() {
+        HystrixMetricsStreamServlet hystrixMetricsStreamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean<HystrixMetricsStreamServlet> servletRegistrationBean = new ServletRegistrationBean<HystrixMetricsStreamServlet>();
+        servletRegistrationBean.setServlet(hystrixMetricsStreamServlet);
+        servletRegistrationBean.addUrlMappings("/hystrix.stream");
+        servletRegistrationBean.setName("hystrixMetricsStreamServlet");
+        return servletRegistrationBean;
+    }
+
+}
