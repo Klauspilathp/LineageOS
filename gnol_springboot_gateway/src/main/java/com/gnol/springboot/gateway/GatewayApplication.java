d9286bd7f07c8fd80fd15f2537b73b0c3f726c04
Klauspilathp <zhouchekui05492@126.com>
2025-12-24 01:34:56 +0800

    1

---
+++ gnol_springboot_gateway/src/main/java/com/gnol/springboot/gateway/GatewayApplication.java
@@ -0,0 +1,24 @@
+package com.gnol.springboot.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+/**
+ * @Title: GatewayApplication
+ * @Package: com.gnol.springboot.gateway
+ * @author: 吴佳隆
+ * @date: 2020年6月27日 下午7:03:08
+ * @Description: gateway 网关启动类
+ */
+@SpringBootApplication(scanBasePackages = {"com.gnol.springboot"})
+@EnableDiscoveryClient // eureka 客户端
+@EnableFeignClients // 启用 Fegin
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+
+}
