2080bcc7e93da4bbb994386fae2a4a58c86a114d
Klauspilathp <zhouchekui05492@126.com>
2026-03-07 21:11:26 +0800

    1

--- gnol_springboot_zuul_gateway/src/main/java/com/gnol/springboot/gateway/filters/PreAuthFilter.java
+++ gnol_springboot_zuul_gateway/src/main/java/com/gnol/springboot/gateway/filters/PreAuthFilter.java
@@ -4,8 +4,16 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.netflix.zuul.filters.support.FilterConstants;
+import org.springframework.stereotype.Component;
 
+import com.gnol.plugins.core.PageData;
+import com.gnol.plugins.core.PageResult;
+import com.gnol.plugins.core.StringUtil;
+import com.gnol.plugins.core.exception.AuthRuntimeException;
+import com.gnol.springboot.common.constant.AuthConstant;
+import com.gnol.springboot.common.feigns.auth.AuthFeignClient;
 import com.netflix.zuul.ZuulFilter;
 import com.netflix.zuul.context.RequestContext;
 import com.netflix.zuul.exception.ZuulException;
@@ -18,14 +26,22 @@ import com.netflix.zuul.exception.ZuulException;
  * @Description: zuul 认证前置过滤器，Spring Cloud 为 zuul 在 spring-cloud-netflix-core 包中默认启用了一些过滤器。
  * 如果要禁用过滤器，可以在属性文件中配置 zuul.<SimpleClassName>.<FilterType>.disable=true，例如 zuul.PreRequestLogFilter.pre.disable=true
  */
+@Component
 public class PreAuthFilter extends ZuulFilter {
     private static final Logger logger = LoggerFactory.getLogger(PreAuthFilter.class);
+    @Autowired
+    private AuthFeignClient authFeignClient;
 
     /**
      * 是否执行该过滤器，true 表示执行
      */
     @Override
     public boolean shouldFilter() {
+        RequestContext ctx = RequestContext.getCurrentContext();
+        HttpServletRequest request = ctx.getRequest();
+        if (StringUtil.startsWith(request.getServletPath(), "/auth")) {
+            return false;
+        }
         return true;
     }
 
@@ -36,8 +52,39 @@ public class PreAuthFilter extends ZuulFilter {
     public Object run() throws ZuulException {
         RequestContext ctx = RequestContext.getCurrentContext();
         HttpServletRequest request = ctx.getRequest();
-        logger.info(String.format("send %s request to %s", request.getMethod(), request.getRequestURL().toString()));
+        PageData pd = new PageData();
+        pd.put("servlet_path", request.getServletPath());
+        pd.put("token", getTokenByRequest(request));
+        PageResult result = authFeignClient.validate(pd);
+        if (!result.isOk()) {
+            logger.debug("{} 路径的接口被 token {} 的用户非法访问...", pd.get("servlet_path"), pd.get("token"));
+            throw new AuthRuntimeException(result.getMessage());
+        }
+        // return 值没有意义，zuul 框架没有使用
         return null;
+    }
+
+    /**
+     * @Title: getTokenByRequest
+     * @author: 吴佳隆
+     * @data: 2020年6月28日 上午11:51:02
+     * @Description: 获取请求中的 token
+     * @param request   javax.servlet.http.HttpServletRequest 对象
+     * @return String   token 字符串
+     */
+    private String getTokenByRequest(HttpServletRequest request) {
+        String token = request.getHeader(AuthConstant.AUTH_HEADER);
+        if (StringUtil.isBlank(token)) {
+            // 获取 token 请求头中的 token
+            token = request.getHeader(AuthConstant.TOKEN);
+            if (StringUtil.isBlank(token)) {
+                // 从参数中获取 token
+                token = request.getParameter(AuthConstant.TOKEN);
+            }
+        } else {
+            token = token.substring("Bearer ".length());
+        }
+        return token;
     }
 
     /**
@@ -53,7 +100,7 @@ public class PreAuthFilter extends ZuulFilter {
      */
     @Override
     public int filterOrder() {
-        return FilterConstants.PRE_DECORATION_FILTER_ORDER + 2;
+        return FilterConstants.PRE_DECORATION_FILTER_ORDER + 1;
     }
 
 }
