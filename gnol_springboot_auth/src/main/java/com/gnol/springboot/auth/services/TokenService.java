d73798b39cb736dc35cd6b111f176b528bf3fee6
Klauspilathp <zhouchekui05492@126.com>
2026-03-17 02:37:56 +0800

    1

--- gnol_springboot_auth/src/main/java/com/gnol/springboot/auth/services/TokenService.java
+++ gnol_springboot_auth/src/main/java/com/gnol/springboot/auth/services/TokenService.java
@@ -1,5 +1,9 @@
 package com.gnol.springboot.auth.services;
 
+import java.util.Map;
+
+import com.gnol.plugins.core.PageData;
+
 /**
  * @Title: TokenService
  * @Package: com.gnol.springboot.auth.services
@@ -8,5 +12,35 @@ package com.gnol.springboot.auth.services;
  * @Description: token 服务接口
  */
 public interface TokenService {
+
+    /**
+     * @Title: getToken
+     * @author: 吴佳隆
+     * @data: 2020年6月29日 下午12:27:22
+     * @Description: 获取 token
+     * @param claims    载荷
+     * @return String
+     */
+    String getToken(Map<String, Object> claims);
+
+    /**
+     * @Title: validateToken
+     * @author: 吴佳隆
+     * @data: 2020年6月29日 下午12:27:37
+     * @Description: 验证 token 是否有效
+     * @param pd
+     * @return boolean
+     */
+    boolean validateToken(PageData pd);
+
+    /**
+     * @Title: updateToken
+     * @author: 吴佳隆
+     * @data: 2020年6月29日 下午12:31:27
+     * @Description: 刷新 token
+     * @param pd
+     * @return String
+     */
+    String updateToken(PageData pd);
 
 }
--- gnol_springboot_auth/src/main/java/com/gnol/springboot/auth/services/impl/JwtTokenServiceImpl.java
+++ gnol_springboot_auth/src/main/java/com/gnol/springboot/auth/services/impl/JwtTokenServiceImpl.java
@@ -1,6 +1,14 @@
 package com.gnol.springboot.auth.services.impl;
 
+import java.util.Map;
+
+import javax.annotation.Resource;
+
+import com.gnol.jwt.spring.boot.autoconfigure.JwtUtil;
+import com.gnol.plugins.core.PageData;
 import com.gnol.springboot.auth.services.TokenService;
+
+import io.jsonwebtoken.JwtException;
 
 /**
  * @Title: JwtTokenServiceImpl
@@ -10,5 +18,42 @@ import com.gnol.springboot.auth.services.TokenService;
  * @Description: jwt token 服务实现
  */
 public class JwtTokenServiceImpl implements TokenService {
+    /**
+     * jwt token 工具类
+     */
+    @Resource(name = "jwtUtil")
+    private JwtUtil jwtUtil;
+
+    @Override
+    public String getToken(Map<String, Object> claims) {
+        String token = null;
+        if (claims == null || claims.isEmpty()) {
+            token = jwtUtil.generateToken(jwtUtil.getJwtProperties().getSubject(),
+                    jwtUtil.getJwtProperties().getSecret(), jwtUtil.getJwtProperties().getExpiration());
+        } else {
+            Object subject = claims.remove("subject");
+            token = jwtUtil.generateToken(claims,
+                    subject == null ? jwtUtil.getJwtProperties().getSubject() : subject.toString(),
+                    jwtUtil.getJwtProperties().getSecret(), jwtUtil.getJwtProperties().getExpiration());
+        }
+        return token;
+    }
+
+    @Override
+    public boolean validateToken(PageData pd) {
+        return jwtUtil.isTokenExpired(pd.getString("token"), jwtUtil.getJwtProperties().getSecret());
+    }
+
+    @Override
+    public String updateToken(PageData pd) {
+        String token = "";
+        try {
+            token = jwtUtil.updateToken(pd.getString("token"), jwtUtil.getJwtProperties().getSecret(),
+                    jwtUtil.getJwtProperties().getExpiration());
+        } catch (JwtException e) {
+            e.printStackTrace();
+        }
+        return token;
+    }
 
 }
