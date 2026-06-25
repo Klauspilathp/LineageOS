3b5b79c08721ec8a06197f203808e664cf165c84
Klauspilathp <zhouchekui05492@126.com>
2026-06-26 00:57:57 +0800

    1

---
+++ gnol_springboot_client_web/src/main/java/com/gnol/springboot/client/config/AopAutoConfiguration.java
@@ -0,0 +1,19 @@
+package com.gnol.springboot.client.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+/**
+ * @Title: AopAutoConfiguration
+ * @Package: com.gnol.springboot.client.config
+ * @author: 吴佳隆
+ * @date: 2020年7月2日 下午2:26:53
+ * @Description: aop 自动代理
+ */
+@Configuration
+@EnableAspectJAutoProxy(proxyTargetClass = true)
+public class AopAutoConfiguration {
+
+    // 设置需要初始化的 bean
+
+}
